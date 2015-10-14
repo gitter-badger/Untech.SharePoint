fb38e48d774eb10a03af885de43e473462dbf61b
Siarhei Svirski <zversergey@tut.by>
2015-10-14 11:33:33 +0300

    few more changes

--- Untech.SharePoint.Mappings.ClassLike/ClassLikeContextMapping.cs
+++ Untech.SharePoint.Mappings.ClassLike/ClassLikeContextMapping.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq.Expressions;
 using System.Reflection;
+using Untech.SharePoint.Common.Converters;
 using Untech.SharePoint.Common.Data;
 using Untech.SharePoint.Common.Mappings;
 using Untech.SharePoint.Common.MetaModels;
@@ -10,6 +12,7 @@ namespace Untech.SharePoint.Mappings.ClassLike
 	internal class ClassLikeMappingSource<TContext> : IMappingSource<TContext>
 		where TContext : ISpContext
 	{
+
 		public MetaContext GetMetaContext()
 		{
 			throw new NotImplementedException();
@@ -23,13 +26,57 @@ namespace Untech.SharePoint.Mappings.ClassLike
 		public Type ContextType { get; private set; }
 	}
 
-	internal class ClassLikeContextMapping :  IMetaContextProvider
+	internal class ContextMap<TContext> :  IMetaContextProvider
+		where TContext : ISpContext
 	{
 		public MetaContext GetMetaContext()
 		{
 			throw new NotImplementedException();
 		}
+
+		public ListPart List<TEntity>(Expression<Func<TContext, ISpList<TEntity>>> property, ContentTypeMap<TEntity> contentType)
+		{
+			throw new NotImplementedException();
+		}
 	}
 
-	
+	internal class ListPart: IMetaListProvider
+	{
+		public ListPart Title(string title)
+		{
+			throw new NotImplementedException();
+
+			return this;
+		}
+	}
+
+
+	internal class ContentTypeMap<TEntity> : IMetaContentTypeProvider
+	{
+
+		public FieldPart Field<T>(Expression<Func<TEntity, T>> property)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+
+	internal class FieldPart : IMetaFieldProvider
+	{
+		public FieldPart InternalName(string internalName)
+		{
+			throw new NotImplementedException();
+		}
+
+		public FieldPart TypeAsString(string typeAsString)
+		{
+			throw new NotImplementedException();
+		}
+
+		public FieldPart CustomConverter<TConverter>()
+			where TConverter : IFieldConverter, new() 
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
--- Untech.SharePoint.Mappings.ClassLike/MappingsExtension.cs
+++ Untech.SharePoint.Mappings.ClassLike/MappingsExtension.cs
@@ -5,7 +5,7 @@ namespace Untech.SharePoint.Mappings.ClassLike
 {
 	public static class MappingsExtension
 	{
-		public static IMappingSource<TContext> ClassLike<TContext>(this Common.Mappings.Mappings mappings)
+		public static IMappingSource<TContext> ClassLike<TContext>(this Common.Mappings.Mappings mappings, ContextMap contextMap)
 		   where TContext : ISpContext
 		{
 			return new ClassLikeMappingSource<TContext>();
