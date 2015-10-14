f4ae40b1fbfcf8fd6cfc9d68b1cf81a661a36e47
Siarhei Svirski <zversergey@tut.by>
2015-10-14 11:12:29 +0300

    class like mapping init

---
+++ Untech.SharePoint.Mappings.ClassLike/ClassLikeContextMapping.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using Untech.SharePoint.Common.Data;
+using Untech.SharePoint.Common.Mappings;
+using Untech.SharePoint.Common.MetaModels;
+using Untech.SharePoint.Common.MetaModels.Providers;
+
+namespace Untech.SharePoint.Mappings.ClassLike
+{
+	internal class ClassLikeMappingSource<TContext> : IMappingSource<TContext>
+		where TContext : ISpContext
+	{
+		public MetaContext GetMetaContext()
+		{
+			throw new NotImplementedException();
+		}
+
+		public string GetListTitleFromContextMember(MemberInfo member)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Type ContextType { get; private set; }
+	}
+
+	internal class ClassLikeContextMapping :  IMetaContextProvider
+	{
+		public MetaContext GetMetaContext()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	
+}
---
+++ Untech.SharePoint.Mappings.ClassLike/MappingsExtension.cs
@@ -0,0 +1,14 @@
+﻿using Untech.SharePoint.Common.Data;
+using Untech.SharePoint.Common.Mappings;
+
+namespace Untech.SharePoint.Mappings.ClassLike
+{
+	public static class MappingsExtension
+	{
+		public static IMappingSource<TContext> ClassLike<TContext>(this Common.Mappings.Mappings mappings)
+		   where TContext : ISpContext
+		{
+			return new ClassLikeMappingSource<TContext>();
+		}
+	}
+}
